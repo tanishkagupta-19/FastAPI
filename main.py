abd55b0f55cb4fce8bff7d5e542cb90ca1e61d83
tanishkagupta-19 <tanishhkkaaa@gmail.com>
2025-11-18 01:46:53 +0530

    feat: added about and view endpoints

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+        return data
+    
+@app.get("/")
+def hello():
+    return {"message": "Hello, World!"}
+
+@app.get("/about")
+def about():
+    return {"message": "This is fully functional API to manage your patiend records."}
+
+@app.get('/view')
+def view():
+    data=load_data()
+    return data
